3f0d9e91329c355be8f515cc9ed7eea02ad48504
Carlosmicael <116389799+Carlosmicael@users.noreply.github.com>
2023-07-18 05:59:50 +0200

    Add files via upload

---
+++ problema02/src/SRP_CP/Televisor.java
@@ -0,0 +1,22 @@
+package SRP_CP;
+import java.util.ArrayList;
+public class Televisor {
+    private String marca;
+    private double precio;
+    public void establecerMarca(String m) {
+        marca = m;
+    }
+    public void establecerPrecio(double p) {
+        precio = p;
+    }
+    public String obtenerMarca() {
+        return marca;
+    }
+    public double obtenerPrecio() {
+        return precio;
+    }
+    @Override
+    public String toString() {
+        return String.format("TV:%s\n%s\n", obtenerMarca(), obtenerPrecio());
+    }
+}
